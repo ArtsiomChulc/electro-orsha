c27fa5ea18a7651795b721f070c1a9003d7a5c46
ArtsiomChults <artemorsha@mail.ru>
2025-06-19 14:53:27 +0300

    fix: settings project

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
 
 export default defineConfig({
-    plugins: [
-        react(),
-        tsconfigPaths(),
-    ],
-    resolve: {
-        alias: {
-            '@src': resolve(__dirname, 'src'),
-            '@': resolve(__dirname, 'src'),
-        },
-    },
+    plugins: [react(), tsconfigPaths()],
 });
