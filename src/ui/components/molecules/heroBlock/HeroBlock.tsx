8eddb9fae777d413f531df0a6885a95d3675a68c
ArtsiomChults <artemorsha@mail.ru>
2025-07-10 14:43:03 +0300

    add: navigate for btn

--- src/ui/components/molecules/heroBlock/HeroBlock.tsx
+++ src/ui/components/molecules/heroBlock/HeroBlock.tsx
@@ -1,11 +1,18 @@
 import { useAppSelector } from '@/app/hooks/hooks';
+import { PATH } from '@/app/paths/paths';
 import heroImg from '@/assets/img/hero_image.png';
 import { Image } from '@/ui/components/atoms/image/Image';
 import { Loader } from '@/ui/components/atoms/loader/Loader';
+import { useNavigate } from 'react-router';
 import s from './HeroBlock.module.css';
 
 export const HeroBlock = () => {
     const loading = useAppSelector(state => state.admin.isLoading);
+    const navigate = useNavigate();
+
+    const onHandlerButton = () => {
+        navigate(PATH.contact, { replace: true });
+    };
 
     if (loading)
         return (
@@ -19,6 +26,7 @@ export const HeroBlock = () => {
                 title={'БЕЗОПАСНО И НАДЕЖНО'}
                 subtitle={'Мы — сервис, которому вы можете доверять'}
                 buttonLabel={'Связаться с нами'}
+                onButtonClick={onHandlerButton}
             />
         </div>
     );
