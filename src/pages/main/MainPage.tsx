e4df11469c47b2c416f598edeb85b3878c9f1951
ArtsiomChults <artemorsha@mail.ru>
2025-07-22 15:59:02 +0300

    feat: get actual price

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,9 +1,11 @@
+import { usePriceInfo } from '@/features/price/helpers/usePriceInfo';
 import { db } from '@/firebase';
 import { HeroBlock } from '@/ui/components/molecules/heroBlock/HeroBlock';
 import { collection, getDocs } from 'firebase/firestore';
 import { useEffect } from 'react';
 
 export const MainPage = () => {
+    usePriceInfo();
     useEffect(() => {
         const getCol = async () => {
             const snapshot = await getDocs(collection(db, 'hero_block'));
